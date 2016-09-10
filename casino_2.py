0aba644068e759b004820a7d726e70d1c92aca06
GGfrerer <ggfrerer@gmail.com>
2016-09-10 13:46:30 +0200

    Korrektur Code Schleife casino_2

--- casino_2.py
+++ casino_2.py
@@ -5,11 +5,10 @@ secret = random.randint(1,n)
 guess = 0
 while guess != secret:
     guess = int(input("Erraten Sie die geheime Zahl (1-100): "))
-    if guess > 0:
-        if guess > secret:
-            print("Die Zahl ist zu hoch. Versuchen Sie es noch einmal.")
-        elif guess < secret:
-            print("Die Zahl ist zu klein. Versuchen Sie es noch einmal.")
+    if guess > secret:
+        print("Die Zahl ist zu hoch. Versuchen Sie es noch einmal.")
+    elif guess < secret:
+        print("Die Zahl ist zu klein. Versuchen Sie es noch einmal.")
 else:
     print("Gratulation. Sie haben richtig geraten!!!")
     
