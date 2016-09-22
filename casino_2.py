e0ad6dd7e49c5e26116a90ad266840d491f88eff
GGfrerer <ggfrerer@gmail.com>
2016-09-22 10:27:40 +0200

    main function added

--- casino_2.py
+++ casino_2.py
@@ -2,13 +2,17 @@ import random
 n = 100
 secret = random.randint(1,n)
 
-guess = 0
-while guess != secret:
-    guess = int(input("Erraten Sie die geheime Zahl (1-100): "))
-    if guess > secret:
-        print("Die Zahl ist zu hoch. Versuchen Sie es noch einmal.")
-    elif guess < secret:
-        print("Die Zahl ist zu klein. Versuchen Sie es noch einmal.")
-else:
-    print("Gratulation. Sie haben richtig geraten!!!")
-    
+
+def main():
+    guess = 0
+    while guess != secret:
+        guess = int(input("Erraten Sie die geheime Zahl (1-100): "))
+        if guess > secret:
+            print("Die Zahl ist zu hoch. Versuchen Sie es noch einmal.")
+        elif guess < secret:
+            print("Die Zahl ist zu klein. Versuchen Sie es noch einmal.")
+    else:
+        print
+        print("Gratulation. Sie haben richtig geraten!!!")
+if __name__ == "__main__":
+    main()
